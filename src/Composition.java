1b142ba3bda6be553db648b5c6f8c50ab9d8ea48
MFatoni <fatoni.hello@gmail.com>
2023-12-18 10:52:47 +0700

    add composition and encapsulation

---
+++ src/Composition.java
@@ -0,0 +1,84 @@
+public class Composition {
+    public static void main(String[] args) {
+        ComputerCase theCase = new ComputerCase("2208","240");
+        Monitor theMonitor = new Monitor("27inch", "Acer",27,"2540 x 1440");
+        Motherboard theMotherBoard = new Motherboard("BJ-200", "Asus",4,6,"v2.44");
+        CompositionPersonalComputer thePC = new CompositionPersonalComputer("2208","Dell", theCase, theMonitor, theMotherBoard);
+
+//        thePC.getMonitor().drawPixelAt(10,10,"black");
+//        thePC.getMotherboard().loadProgram("Windows OS");
+//        thePC.getComputerCase().pressPowerButton();
+
+        thePC.powerUp();
+
+        CompositionSmartKitchen kitchen = new CompositionSmartKitchen();
+
+//        kitchen.getDishWasher().setHasWorkToDo(true);
+//        kitchen.getIceBox().setHasWorkToDo(true);
+//        kitchen.getBrewMaster().setHasWorkToDo(true);
+//
+//        kitchen.getDishWasher().doDishes();
+//        kitchen.getIceBox().orderFood();
+//        kitchen.getBrewMaster().brewCoffee();
+
+        kitchen.setKitchenState(true, false, true);
+        kitchen.doKitchenWork();
+    }
+}
+/*
+inheritance defines an is a relationship
+composition defines a has a relationship
+
+Product
+^ inherit (is a)
+PersonalComputer
+^ is made up of (has a)
+-> Monitor
+-> Motherboard
+-> ComputerCase
+
+Monitor
+size:int
+resolution:String
+drawPixelAt(int x, int y, String color)
+
+Motherboard
+ramSlots:int
+cardSlots:int
+bios:String
+loadProgram(String programName)
+
+ComputerCase
+powerSupply:String
+pressPowerButton()
+
+inheritance is a way to reuse functionality and attributes
+composition is a way to make the combination of classes, act like a single coherent object
+
+the reasons composition is preferred over inheritance:
+-> composition is more flexible, you can add parts in or remove them, and these changes are less likely to have downstream effect
+-> composition provides functional reuse outside of the class hierarchy,
+meaning classes can share attributes & behaviour, by having similar components
+instead of inheriting functionality from a parent or base class
+-> java's inheritance breaks encapsulation, because parents state or behavior
+
+inheritance is less flexible
+-> adding a class to, or removing a class from, a class hierarchy, may impact all subclasses from that point
+-> in addition, a new subclass may not need all the functionality or attributes of its parent class
+
+-> product
+model:String
+manufacturer: String
+^inherits (is a)
+-> motherboard
+ramSlots:int
+cardSlots:intW
+bios:String
+dimensions:Dimensions
+loadProgram(String programName)
+^ has a
+-> Dimensions
+width:int
+height:int
+depth:int
+*/
---
+++ src/Encapsulation.java
@@ -0,0 +1,38 @@
+public class Encapsulation {
+    public static void main(String[] args) {
+        EncapsulationPlayer player = new EncapsulationPlayer();
+        player.name = "Tim";
+        player.health = 20;
+        player.weapon = "Sword";
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = "+ player.healthRemaining());
+
+        player.loseHealth(11);
+        System.out.println("Remaining health = "+ player.healthRemaining());
+
+        EncapsulationEnchancedPlayer tim = new EncapsulationEnchancedPlayer("Tim");
+        System.out.println("Initial health = "+ tim.healthRemaining());
+
+    }
+}
+
+/*
+encapsulation
+-> to make the interface simpler, we may want to hide unnecessary details
+-> to protect the integrity of data on an object, we may hide or restrict access to some of the data and operations
+-> to decouple the published interface from the internal details of the class, we may hide actual names and types of class members
+-> allowing direct access to data on an object, can potentially bypass checks, and additional processing, your class has in place to manage the data
+-> it encourages an interdependency, or coupling, between the calling code and the class
+-> allowing direct access to fields, means calling code would need to change, when you edit any of the fields
+-> omitting a constructor, that would accept initialization data, may mean the calling code is responsible for setting up this data, on the new object
+-> changing a field name, broke the calling code
+-> calling code had to take on the responsibility, for properly initializing a new player
+
+encapsulation principles
+-> create constructors for object initialization, which enforces that only object with valid data will get created
+-> use the private access modifier for your fields
+-> use setter and getter method sparingly, and only as needed
+-> use access modifiers that aren't private only for the methods that the calling code needs to use
+*/
