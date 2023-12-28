26ecd4298c021b100398b56ddd91b1650d88a80e
MFatoni <mohfatoni@yahoo.com>
2023-12-28 12:16:04 +0700

    add enum

---
+++ src/TypeEnum.java
@@ -0,0 +1,17 @@
+public class TypeEnum {
+    public static void main(String[] args) {
+        TypeEnumDays weekDay = TypeEnumDays.MON;
+        System.out.println(weekDay);
+        System.out.println(weekDay.name());
+        System.out.println(weekDay.ordinal());
+
+        for (TypeEnumTopping topping : TypeEnumTopping.values()){
+            System.out.println(topping.name() +" : "+ topping.getPrice());
+        }
+    }
+}
+/*
+enum
+-> special data type that contains predefined constants
+-> constant is a variable whose value can't be changed, once it's value has been assigned
+ */
---
+++ src/TypeEnumDays.java
@@ -0,0 +1,3 @@
+public enum TypeEnumDays {
+     SUN, MON, TUES, WED, THURS, FRI, SAT;
+}
